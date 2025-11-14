ae7f044763db0edc55f6b01d165d4c2bccb9c22e
IgorKulduaev <and.sobol39@mail.ru>
2025-11-14 11:24:58 +0200

    Update Program.cs

--- Lab4/Lab4/Program.cs
+++ Lab4/Lab4/Program.cs
@@ -23,46 +23,46 @@
 //Console.WriteLine($"Процент чисел, превышающих среднее: {procent:F2} %");
 
 //Двумерные массивы, 9 Вариант, Базовый уровень
-//int[,] array = {
-//            { 1, 3, 5, 7, 9, 2 },
-//            { 4, 6, 8, 10, 12, 14 },
-//            { 13, 11, 9, 7, 5, 3 },
-//            { 2, 4, 6, 8, 10, 12 },
-//            { 15, 13, 11, 9, 7, 5 }
-//        };
+//     int[,] array = new int[5, 6];   
+//     Random rand = new Random();
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j] = rand.Next(10);
+//             Console.Write(array[i, j] + " ");
+//         }
 
-//int rows = array.GetLength(0);
-//int cols = array.GetLength(1);
+//         int rows = array.GetLength(0);
+//     int cols = array.GetLength(1);
 
-//// Вычисление среднего арифметического каждого столбца
-//Console.WriteLine("Среднее арифметическое каждого столбца:");
-//for (int col = 0; col < cols; col++)
-//{
-//    double sum = 0;
-//    for (int row = 0; row < rows; row++)
-//    {
-//        sum += array[row, col];
-//    }
-//    double average = sum / rows;
-//    Console.WriteLine($"Столбец {col + 1}: {average:F2}");
-//}
+//     // Вычисление среднего арифметического каждого столбца
+//     Console.WriteLine("Среднее арифметическое каждого столбца:");
+//     for (int col = 0; col < cols; col++)
+//     {
+//         double sum = 0;
+//         for (int row = 0; row < rows; row++)
+//         {
+//             sum += array[row, col];
+//         }
+//         double average = sum / rows;
+//         Console.WriteLine($"Столбец {col + 1}: {average:F2}");
+//     }
 
-//Console.WriteLine();
+//     Console.WriteLine();
 
-//// Поиск максимума и минимума в каждой строке
-//for (int row = 0; row < rows; row++)
-//{
-//    int max = array[row, 0];
-//    int min = array[row, 0];
-
-//    for (int col = 1; col < cols; col++)
-//    {
-//        if (array[row, col] > max)
-//            max = array[row, col];
-//        if (array[row, col] < min)
-//            min = array[row, col];
-//    }
-
-//    Console.WriteLine($"Строка {row + 1} - Максимум: {max}, Минимум: {min}");
-//}
-
+//     // Поиск максимума и минимума в каждой строке
+//     for (int row = 0; row < rows; row++)
+//     {
+//         int max = array[row, 0];
+//         int min = array[row, 0];
+//         for (int col = 1; col < cols; col++)
+//         {
+//             if (array[row, col] > max)
+//                 max = array[row, col];
+//             if (array[row, col] < min)
+//                 min = array[row, col];
+//         }
+//         Console.WriteLine($"Строка {row + 1} - Максимум: {max}, Минимум: {min}");
+//     }
+// }
